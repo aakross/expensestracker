031248319751843a9991164e9c1b9d34b11e7165
sistemas <ti.espiral@gmail.com>
2024-04-30 13:55:53 -0600

    30/04/24

--- lib/widgets/new_expenses.dart
+++ lib/widgets/new_expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -20,6 +21,25 @@ class _NewExpense extends State<NewExpense> {
 // Se utilizo TextEditingController ya que flutter maneja mejor el como se guarda lo que el usuario escribio
   final _tittleController = TextEditingController();
   final _amountController = TextEditingController();
+  //Aqui se le esta diciendo a Dart que _selectedDate puede contener nada o un valor tipo DateTime
+  DateTime? _selectedDate;
+
+  Category _selectedCategory = Category.leisure;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    //Aqui se le esta agregando el dia actual menos un año
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    //Los async await se pueden utilizar tambien en metodos futuros como showDatePicker
+    final pickDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+    setState(() {
+      _selectedDate = pickDate;
+    });
+  }
 
 //dispose sirve para cerrar el campo textfield de la UI en este caso cerrarlo cuando el modal este abierto
   @override
@@ -42,16 +62,69 @@ class _NewExpense extends State<NewExpense> {
               label: Text('Tittle'),
             ),
           ),
-          TextField(
-            controller: _amountController,
-            keyboardType: TextInputType.number,
-            decoration: const InputDecoration(
-              prefixText: '\$ ',
-              label: Text('Amount'),
-            ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    prefixText: '\$ ',
+                    label: Text('Amount'),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      _selectedDate == null
+                          ? 'No date selected'
+                          //El signo de ! se le agrego ya que dart intuye que sera null pero con ! se le forza porque sabemos que por el operador ternario que no sera asi, en pocas palbaras le decimos a dart que no sera null
+                          : formatter.format(_selectedDate!),
+                    ),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(Icons.calendar_month),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 16,
           ),
           Row(
             children: [
+              //Items quiere una lista por eso se le agrega el map para transformarla en lista
+              DropdownButton(
+                  value: _selectedCategory,
+                  items: Category.values
+                      .map(
+                        (category) => DropdownMenuItem(
+                          //Aqui _selectedCategory servira para guardar internamente el valor seleccionado
+                          value: category,
+                          child: Text(
+                            category.name.toUpperCase(),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) {
+                    //Aqui se esta verificando si value es Null en dado caso se returna y si no cambia el estado por el acutal
+                    if (value == null) {
+                      return;
+                    }
+                    //Si es nulo no se ejecutara el siguiente codigo
+                    setState(() {
+                      _selectedCategory = value;
+                    });
+                  }),
+              const Spacer(),
               TextButton(
                 onPressed: () {
                   Navigator.pop(context);
